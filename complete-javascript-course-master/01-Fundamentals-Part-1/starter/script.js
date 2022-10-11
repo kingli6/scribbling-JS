f33c5561564802d842aa79605aa987ffbb140e94
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-10-11 10:08:13 +0200

    js

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+
+let js = 'amazing';
+
+// if (js == 'amazing') alert('JavaScript is FUn!')
+// console.log(40 + 50 + 39 + 59)
+const current_year = 2022
+let language
+console.log(language) // undefined
+console.log(js, language)
+
+//creating variables
+// you can use var, let or const or no declaration of type when making variables
+//- don't use var. var is dead. Use mostly const, and let only when you know that it will be changed.
+
+const _j = 10
+// _j = 20
+// console.log(_j) //Uncaught TypeError: invalid assignment to const '_j'
+
+joes_bday = current_year - 1988
+console.log(joes_bday + 1, 'plus one', joes_bday - 1, 'another comment', 2 ** 3, '2**3')
+console.log(typeof ('jj'), 'type of string')
+
+let x = 1
+x++;    //x+= 1
+x += 10;
+kiras_bday = current_year - 2018
+
+const isFullAge = kiras_bday >= 4; //you get a boolean
+console.log(isFullAge)
+
+console.log(current_year - 1991 > current_year - 2018, 'result is 4 so True')
+let some_bool = true
