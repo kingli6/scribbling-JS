178f1447106ce39cdf4b710a3295ef03f9bb41f6
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-11-04 18:00:03 +0100

    how to solve a problem

---
+++ Scribbles/11 solving a problem.js
@@ -0,0 +1,14 @@
+/*
+Step 1. Understand the problem.
+    Ask the right questions to get a 
+    clear picture of the problem
+
+Step 2. Dive and conquer. Break big 
+    problems into smaller sub-problems.
+
+Step 3. Don't be afraid to do as much
+    research as you have to.
+
+Step 4. For bigger problems, write pseudo-code
+    before writing the actual code.
+*/
