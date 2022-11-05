ec7375d526fba7bcd146b0fdc661980fe3b1f09d
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-11-05 02:27:47 +0100

    ..

---
+++ Scribbles/12 debugger.js
@@ -0,0 +1,13 @@
+// nice tags
+//console.warn('something)
+//console.error('something)
+
+//DEBUGGING can be found in the source tab
+//  in the browser
+//https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648363#search
+
+//You can also use
+debugger;
+
+// this will give you a table format!
+console.table('object');
