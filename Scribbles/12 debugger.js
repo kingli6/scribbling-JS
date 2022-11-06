143f585d178cdc2d38ee8bf00a2d5d96a27e13b8
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-11-06 14:43:58 +0100

    ..

--- Scribbles/12 debugger.js
+++ Scribbles/12 debugger.js
@@ -2,7 +2,6 @@
 //console.warn('something)
 //console.error('something)
 
-//DEBUGGING can be found in the source tab TODO FixME TITLE
 //  in the browser
 //https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648363#search
 
@@ -11,3 +10,15 @@ debugger;
 
 // this will give you a table format!
 console.table('object');
+
+//DEBUGGING can be found in the source tab TODO FixME TITLE
+/** Open settings, open setting in the json mode. 
+ "todohighlight.isCaseSensitive": true,
+  "todohighlight.keywords": [
+    {
+      // https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648339#overview 15:42
+      "text": "TODO",
+      "colot": "#333",
+      "backgroundColor": "#2ecc71"
+    },
+ */
