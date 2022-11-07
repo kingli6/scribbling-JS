a0a81f17cde15c73a9aa2c6c5dd57768c9d731e7
Jonathan Fisher <jonathan.fshr@gmail.com>
2022-11-07 12:53:46 +0100

    guess my number

--- script.js
+++ script.js
@@ -3,34 +3,61 @@
 JavaScript DOM manupilation
 https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648409#search
 
--Select a DOM object
+--Select a DOM object
 document.querySelector(class item)
 
 -Set a DOM object
 document.querySelector('.message').textContent = 'COrrect Number!';
 
--Grab a value from the form
+--Grab a value from the form
 document.querySelector('.guess').value;
--set value 
+--set value 
 document.querySelector('.guess').value = 23;
 
--Action trigger or EventListener
-
+--Action trigger or EventListener
 document.querySelector('.check').addEventListener('click', function(){
     console.log(document.querySelector('.guess').value);
     console.log(typeof guess); //<--a string
 });
     //.check is a button. When clicked, function is run. 
-*/
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'COrrect Number!';
+--Changing css with querySelector
+ document.querySelector('body').style.backgroundColor = '#60b347'; 
+*/
+
+let score = 0;
+const secretNumber = Math.trunc(Math.random() * 10) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
+  //Not a number
   if (!guess) {
-    document.querySelector('.message').textContent = '!!No number!';
+    document.querySelector('.message').textContent = 'No number!';
+
+    //Correct guess!
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').style.width = '30rem';
+    if (score < 5) {
+      score++;
+      document.querySelector('.score').textContent = score;
+      if (score === 5) {
+        //You've won
+        document.querySelector('.message').textContent = "You've won!";
+        document.querySelector('body').style.backgroundColor = '#60b347';
+      }
+    }
+    //Guess is too high
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    //Guess is too low
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  const secretNumber = Math.trunc(Math.random() * 10) + 1;
 });
